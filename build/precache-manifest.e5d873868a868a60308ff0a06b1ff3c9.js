99bd7953fd82b275396d44612c7ee7145e812a00
Mohammad-Ali-Rauf <m.aliadnanrauf@gmail.com>
2021-12-12 00:18:26 +0500

    -

---
+++ build/precache-manifest.e5d873868a868a60308ff0a06b1ff3c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9558eb47faa4e74e7897c3dfd0ec1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723e446260d85516ce05",
+    "url": "/static/css/main.3573085f.chunk.css"
+  },
+  {
+    "revision": "70751a52da3baa541073",
+    "url": "/static/js/2.dd04f9db.chunk.js"
+  },
+  {
+    "revision": "201176730a5f28812d3f840a63994032",
+    "url": "/static/js/2.dd04f9db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723e446260d85516ce05",
+    "url": "/static/js/main.6c64b728.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
